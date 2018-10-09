c4215f70765733afbb181ae8d3e2366b1ef63c94
linsheng <linsheng@xrjiot.com>
2018-10-09 18:52:24 +0800

    feat(HOME): create Tabbar & Home

---
+++ lib/View/Home/HomeView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
